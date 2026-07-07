bcc0873581defc7ed1fd77d8d971da5790d5d901
Juan Gomes Macario <jgomesmacario@outlook.com>
2026-07-07 12:49:24 -0300

    Build stock kernel with crashunix author customizations

--- fs/exec.c
+++ fs/exec.c
@@ -1911,24 +1911,13 @@ int do_execve_file(struct file *file, void *__argv, void *__envp)
 	return __do_execve_file(AT_FDCWD, NULL, argv, envp, 0, file);
 }
 
-#ifdef CONFIG_KSU_MANUAL_HOOK
-__attribute__((hot))
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
-				void *argv, void *envp, int *flags);
-#endif
-
 int do_execve(struct filename *filename,
 	const char __user *const __user *__argv,
 	const char __user *const __user *__envp)
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
-	int fd = AT_FDCWD;
-	int flags = 0;
-#ifdef CONFIG_KSU_MANUAL_HOOK
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-#endif
-	return do_execveat_common(fd, filename, argv, envp, flags);
+	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
 int do_execveat(int fd, struct filename *filename,
@@ -1938,9 +1927,7 @@ int do_execveat(int fd, struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
-#ifdef CONFIG_KSU_MANUAL_HOOK
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-#endif
+
 	return do_execveat_common(fd, filename, argv, envp, flags);
 }
 
@@ -1957,12 +1944,7 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
-	int fd = AT_FDCWD;
-	int flags = 0;
-#ifdef CONFIG_KSU_MANUAL_HOOK
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-#endif
-	return do_execveat_common(fd, filename, argv, envp, flags);
+	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
 static int compat_do_execveat(int fd, struct filename *filename,
@@ -1978,9 +1960,6 @@ static int compat_do_execveat(int fd, struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
-#ifdef CONFIG_KSU_MANUAL_HOOK
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-#endif
 	return do_execveat_common(fd, filename, argv, envp, flags);
 }
 #endif
